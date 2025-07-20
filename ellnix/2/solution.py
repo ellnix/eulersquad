355f6b38618be019aaf449fefb24d3ffc3f91069
ellnix <ellnix@disroot.org>
2025-07-20 12:47:15 +0200

    Solve problem 2 using python itertools

--- ellnix/2/solution.py
+++ ellnix/2/solution.py
@@ -1,13 +1,9 @@
+from itertools import takewhile
+
 def fibonacci(x = 1, y = 2):
     yield x
     yield from fibonacci(y, x + y)
 
-sum = 0
+result = sum(filter(lambda x: x % 2 == 0, takewhile(lambda x: x < 4_000_000, fibonacci())))
 
-for n in fibonacci():
-    if n >= 4_000_000:
-        break
-    elif n % 2 == 0:
-        sum += n
-
-print(sum)
+print(result)
